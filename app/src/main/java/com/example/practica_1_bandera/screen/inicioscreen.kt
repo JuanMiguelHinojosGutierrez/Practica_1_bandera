9971f9f485140fc48986904c7692d4509fbf79a9
Miguel Hinojos <taxzo1126@gmail.com>
2025-09-12 12:14:45 -0600

    Initial commit

---
+++ app/src/main/java/com/example/practica_1_bandera/screen/inicioscreen.kt
@@ -0,0 +1,13 @@
+package com.example.practica_1_bandera.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun  InicioScreen(){
+
+    Box(){
+        Text(text = "Inicio Screen")
+    }
+}
