85a6d12d3f71ca7b069270e1bce17bbff37802a1
Miguel Hinojos <taxzo1126@gmail.com>
2025-09-12 12:24:19 -0600

    creo el modifier

--- app/src/main/java/com/example/practica_1_bandera/screen/inicioscreen.kt
+++ app/src/main/java/com/example/practica_1_bandera/screen/inicioscreen.kt
@@ -3,9 +3,10 @@ package com.example.practica_1_bandera.screen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun  InicioScreen(){
+fun  InicioScreen(modifier: Modifier = Modifier){
 
     Box(){
         Text(text = "Inicio Screen")
